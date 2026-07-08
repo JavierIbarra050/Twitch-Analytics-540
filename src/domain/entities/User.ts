f2fae64fd7288e680b238aff4f9d7e332ede3c52
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-08 10:57:52 +0200

    [#8] - Create IUserRepository and modified User (apikey) (#44)

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -3,21 +3,14 @@ import crypto from 'crypto';
 export class User {
     constructor (
         private readonly email: string,
-        private apiKey?: string,
-    ) {
-        this.apiKey = apiKey || this.generateApiKey();
-    }
-
-    generateApiKey(): string {
-        const newApiKey = crypto.randomBytes(16).toString('hex'); 
-        return newApiKey;   
-    }
+        private apiKey: string,
+    ) { }
 
     public getUserEmail(): string {
         return this.email;
     }
 
-    public getUserApiKey(): string | null {
-        return this.apiKey || null;
+    public getUserApiKey(): string {
+        return this.apiKey;
     }
 }
