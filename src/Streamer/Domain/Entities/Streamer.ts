7439a8ed09b8d028ecb0644c19b5242b836decde
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-08 13:07:22 +0200

    [#52] - Change streamer from class to type

--- src/Streamer/Domain/Entities/Streamer.ts
+++ src/Streamer/Domain/Entities/Streamer.ts
@@ -1,49 +1,11 @@
-export class Streamer {
-    constructor (
-        private readonly id: number,
-        private readonly displayName: string,
-        private readonly type: string,
-        private readonly breadcasterType: string,
-        private readonly description: string,
-        private readonly profileImageUrl: string,
-        private readonly offlineImageUrl: string,
-        private readonly viewCount: number,
-        private readonly createdAt: Date,
-    ) { }
-
-    public getStreamerId(): number {
-        return this.id;
-    }
-
-    public getDisplayName(): string {
-        return this.displayName;
-    }
-
-    public getType(): string {
-        return this.type;
-    }
-
-    public getBreadcasterType(): string {
-        return this.breadcasterType;
-    }
-
-    public getDescription(): string {
-        return this.description;
-    }
-
-    public getProfileImageUrl(): string {
-        return this.profileImageUrl;
-    }
-
-    public getOfflineImageUrl(): string {
-        return this.offlineImageUrl;
-    }
-
-    public getViewCount(): number {
-        return this.viewCount;
-    }
-
-    public getCreatedAt(): Date {
-        return this.createdAt;
-    }
-}
+export type Streamer = {
+    id: number;
+    displayName: string;
+    type: string;
+    breadcasterType: string;
+    description: string;
+    profileImageUrl: string;
+    offlineImageUrl: string;
+    viewCount: number;
+    createdAt: Date;
+};
