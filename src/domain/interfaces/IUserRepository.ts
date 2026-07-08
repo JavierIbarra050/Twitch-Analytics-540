d0a61463e18a97aa63bce0f8383415f23206e99e
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-08 10:58:26 +0200

    Revert "[#8] - Create IUserRepository and modified User (apikey) (#44)"
    This reverts commit f2fae64fd7288e680b238aff4f9d7e332ede3c52.

--- src/domain/interfaces/IUserRepository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "domain/entities/User";
-
-export interface IUserRepository {
-    doesUserAlreadyExists(email: string): boolean;
-    
-    saveUser(email: string, apiKey: string): User; 
-}
