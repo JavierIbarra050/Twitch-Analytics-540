f7aee39df1ea5d548fbb696c9141c34ce1ec9a26
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-09 09:26:40 +0200

    [#26] - Configure stream routes (#63)

--- src/Streamer/Infrastructure/Routes/analyticsRoutes.ts
+++ src/Streamer/Infrastructure/Routes/analyticsRoutes.ts
@@ -1,17 +1,31 @@
 import { Router } from 'express';
 import { TwitchHttpClient } from '../../../Shared/Infrastructure/Twitch/TwitchHttpClient';
+
 import { StreamerTwitchRepository } from '../Repositories/StreamerTwitchRepository';
 import { StreamerService } from '../../Application/Services/StreamerService';
 import { StreamerController } from '../Controllers/StreamerController';
 
+import { StreamTwitchRepository } from '../../../Stream/Infrastructure/Repositories/StreamTwitchRepository';
+import { StreamService } from '../../../Stream/Application/Services/StreamService';
+import { StreamController } from '../../../Stream/Infrastructure/Controller/StreamController';
+
 const router = Router();
 
 const twitchHttpClient = new TwitchHttpClient();
+
+// Streamer
 const streamerRepository = new StreamerTwitchRepository(twitchHttpClient);
 const streamerService = new StreamerService(streamerRepository);
 const streamerController = new StreamerController(streamerService);
 
+// Stream
+const streamRepository = new StreamTwitchRepository(twitchHttpClient);
+const streamService = new StreamService(streamRepository);
+const streamController = new StreamController(streamService);
+
 router.get('/streamer', streamerController.getStreamerById);
+router.get('/streams', streamController.getLiveStreams);
 
 export default router;
+
 
