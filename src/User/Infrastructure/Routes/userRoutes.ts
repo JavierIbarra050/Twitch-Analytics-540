bc15df2d025bd259c3887a8e984384836b726c5a
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-09 11:57:03 +0200

    [#15] - Configure token route

--- src/User/Infrastructure/Routes/userRoutes.ts
+++ src/User/Infrastructure/Routes/userRoutes.ts
@@ -1,14 +1,21 @@
 import { Router } from 'express';
-import { UserSQLiteRepository } from '../Repositories/UserSQLiteRepository';
+import { UserRepositorySQL } from '../Repositories/UserRepositorySQL';
 import { UserService } from '../../Application/Services/UserService';
 import { UserController } from '../Controllers/UserController';
+import { UserTokenService } from '../../Application/Services/UserTokenService';
+import { UserTokenController } from '../Controllers/UserTokenController';
 
 const router = Router();
 
-const userRepository = new UserSQLiteRepository();
+const userRepository = new UserRepositorySQL();
+
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
+const userTokenService = new UserTokenService(userRepository);
+const userTokenController = new UserTokenController(userTokenService);
+
 router.post('/register', userController.register);
+router.post('/token', userTokenController.generateToken);
 
 export default router;
