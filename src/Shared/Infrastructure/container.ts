5ad7a36a1108883299a85c50202ee0d69e22107b
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-10 08:47:53 +0200

    [#110] - Refactor Decouple dependency composition from routing file

---
+++ src/Shared/Infrastructure/container.ts
@@ -0,0 +1,57 @@
+import { TwitchHttpClient } from './Twitch/TwitchHttpClient';
+import { AuthMiddleware } from './Middlewares/AuthMiddleware';
+
+import { StreamerTwitchRepository } from '../../Streamer/Infrastructure/Repositories/StreamerTwitchRepository';
+import { StreamerService } from '../../Streamer/Application/Services/StreamerService';
+import { StreamerController } from '../../Streamer/Infrastructure/Controllers/StreamerController';
+
+import { StreamTwitchRepository } from '../../Stream/Infrastructure/Repositories/StreamTwitchRepository';
+import { StreamService } from '../../Stream/Application/Services/StreamService';
+import { StreamController } from '../../Stream/Infrastructure/Controllers/StreamController';
+
+import { TwitchClient as EnrichedTwitchClient } from '../../EnrichedStream/Infrastructure/Repositories/TwitchClient';
+import { EnrichedStreamService } from '../../EnrichedStream/Application/Services/EnrichedStreamService';
+import { EnrichedStreamController } from '../../EnrichedStream/Infrastructure/Controllers/EnrichedStreamController';
+
+import { TwitchClient as TopOfTheTopsTwitchClient } from '../../TopOfTheTops/Infrastructure/Repositories/TwitchClient';
+import { SQLiteGameCacheRepository } from '../../TopOfTheTops/Infrastructure/Repositories/SQLiteGameCacheRepository';
+import { TopOfTheTopsService } from '../../TopOfTheTops/Application/Services/TopOfTheTopsService';
+import { TopOfTheTopsController } from '../../TopOfTheTops/Infrastructure/Controllers/TopOfTheTopsController';
+
+import { UserRepositorySQL } from '../../User/Infrastructure/Repositories/UserRepositorySQL';
+import { UserService } from '../../User/Application/Services/UserService';
+import { UserController } from '../../User/Infrastructure/Controllers/UserController';
+import { UserTokenService } from '../../User/Application/Services/UserTokenService';
+import { UserTokenController } from '../../User/Infrastructure/Controllers/UserTokenController';
+
+// Shared
+export const twitchHttpClient = new TwitchHttpClient();
+export const authMiddleware = new AuthMiddleware();
+
+// Streamer
+export const streamerRepository = new StreamerTwitchRepository(twitchHttpClient);
+export const streamerService = new StreamerService(streamerRepository);
+export const streamerController = new StreamerController(streamerService);
+
+// Stream
+export const streamRepository = new StreamTwitchRepository(twitchHttpClient);
+export const streamService = new StreamService(streamRepository);
+export const streamController = new StreamController(streamService);
+
+// EnrichedStream
+export const enrichedStreamClient = new EnrichedTwitchClient(twitchHttpClient);
+export const enrichedStreamService = new EnrichedStreamService(enrichedStreamClient);
+export const enrichedStreamController = new EnrichedStreamController(enrichedStreamService);
+
+// TopOfTheTops
+export const topOfTheTopsTwitchClient = new TopOfTheTopsTwitchClient(twitchHttpClient);
+export const gameCacheRepository = new SQLiteGameCacheRepository();
+export const topOfTheTopsService = new TopOfTheTopsService(topOfTheTopsTwitchClient, gameCacheRepository);
+export const topOfTheTopsController = new TopOfTheTopsController(topOfTheTopsService);
+
+// User
+export const userRepository = new UserRepositorySQL();
+export const userService = new UserService(userRepository);
+export const userController = new UserController(userService);
+export const userTokenService = new UserTokenService(userRepository);
+export const userTokenController = new UserTokenController(userTokenService);
--- src/User/Infrastructure/Routes/userRoutes.ts
+++ src/User/Infrastructure/Routes/userRoutes.ts
@@ -1,19 +1,7 @@
 import { Router } from 'express';
-import { UserRepositorySQL } from '../Repositories/UserRepositorySQL';
-import { UserService } from '../../Application/Services/UserService';
-import { UserController } from '../Controllers/UserController';
-import { UserTokenService } from '../../Application/Services/UserTokenService';
-import { UserTokenController } from '../Controllers/UserTokenController';
+import { userController, userTokenController } from '../../../Shared/Infrastructure/container';
 
 const router = Router();
-
-const userRepository = new UserRepositorySQL();
-
-const userService = new UserService(userRepository);
-const userController = new UserController(userService);
-
-const userTokenService = new UserTokenService(userRepository);
-const userTokenController = new UserTokenController(userTokenService);
 
 router.post('/register', userController.register);
 router.post('/token', userTokenController.generateToken);
