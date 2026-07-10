4747a5943666d45d261f4117963d2897f64fd96a
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-07-10 08:48:21 +0200

    [#110] - Refactor Decouple dependency composition from routing file (#125)

--- src/Streamer/Infrastructure/Routes/analyticsRoutes.ts
+++ src/Streamer/Infrastructure/Routes/analyticsRoutes.ts
@@ -1,45 +1,13 @@
 import { Router } from 'express';
-import { TwitchHttpClient } from '../../../Shared/Infrastructure/Twitch/TwitchHttpClient';
-
-import { StreamerTwitchRepository } from '../Repositories/StreamerTwitchRepository';
-import { StreamerService } from '../../Application/Services/StreamerService';
-import { StreamerController } from '../Controllers/StreamerController';
-
-import { StreamTwitchRepository } from '../../../Stream/Infrastructure/Repositories/StreamTwitchRepository';
-import { StreamService } from '../../../Stream/Application/Services/StreamService';
-import { StreamController } from '../../../Stream/Infrastructure/Controllers/StreamController';
-
-import { TwitchClient } from '../../../EnrichedStream/Infrastructure/Repositories/TwitchClient';
-import { EnrichedStreamService } from '../../../EnrichedStream/Application/Services/EnrichedStreamService';
-import { EnrichedStreamController } from '../../../EnrichedStream/Infrastructure/Controllers/EnrichedStreamController';
-import { AuthMiddleware } from '../../../Shared/Infrastructure/Middlewares/AuthMiddleware';
-
-import { SQLiteGameCacheRepository } from '../../../TopOfTheTops/Infrastructure/Repositories/SQLiteGameCacheRepository';
-import { TwitchClient as TopOfTheTopsTwitchClient } from '../../../TopOfTheTops/Infrastructure/Repositories/TwitchClient';
-import { TopOfTheTopsService } from '../../../TopOfTheTops/Application/Services/TopOfTheTopsService';
-import { TopOfTheTopsController } from '../../../TopOfTheTops/Infrastructure/Controllers/TopOfTheTopsController';
+import {
+    authMiddleware,
+    streamerController,
+    streamController,
+    enrichedStreamController,
+    topOfTheTopsController
+} from '../../../Shared/Infrastructure/container';
 
 const router = Router();
-
-const twitchHttpClient = new TwitchHttpClient();
-const authMiddleware = new AuthMiddleware();
-
-const streamerRepository = new StreamerTwitchRepository(twitchHttpClient);
-const streamerService = new StreamerService(streamerRepository);
-const streamerController = new StreamerController(streamerService);
-
-const streamRepository = new StreamTwitchRepository(twitchHttpClient);
-const streamService = new StreamService(streamRepository);
-const streamController = new StreamController(streamService);
-
-const enrichedStreamClient = new TwitchClient(twitchHttpClient);
-const enrichedStreamService = new EnrichedStreamService(enrichedStreamClient);
-const enrichedStreamController = new EnrichedStreamController(enrichedStreamService);
-
-const topOfTheTopsTwitchClient = new TopOfTheTopsTwitchClient(twitchHttpClient);
-const gameCacheRepository = new SQLiteGameCacheRepository();
-const topOfTheTopsService = new TopOfTheTopsService(topOfTheTopsTwitchClient, gameCacheRepository);
-const topOfTheTopsController = new TopOfTheTopsController(topOfTheTopsService);
 
 router.get('/streamer', authMiddleware.execute, streamerController.getStreamerById);
 router.get('/streams', authMiddleware.execute, streamController.getLiveStreams);
